8f0fa0d8235a4ac6907adc4a2156f7505f427e8b
READER123OM <86113292+READER123OM@users.noreply.github.com>
2021-09-20 10:04:24 +0530

    Add files via upload

---
+++ js/board.js
@@ -0,0 +1,22 @@
+Class Board{
+    constructor(x, y, width, height) {
+        const options = {
+          isStatic: true
+        };
+    
+        this.body = Matter.Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.collapse = false;
+        this.image = loadImage("./assets/board.png");
+        World.add(world,this.body)
+    }
+}
+
+display() {
+    var pos = this.body.position;
+     push()
+     imageMode(CENTER);
+     image(this.image,pos.x,pos.y,this.width,this.height);
+     pop()
+}
